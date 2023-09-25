87597c64d24e7cd1bae9c3e17b253534ab65a30b
NotVndtta <thenotalexey192@gmail.com>
2023-09-25 23:16:49 +0300

    Full animation

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 let toastBox = document.getElementById('toastBox');
-let successMsg = 'Successfuly submited';
-let errorMsg = 'Error';
-let invalidMsg = 'You invalid';
+let successMsg = '<i class="fa-solid fa-circle-check"></i> Successfuly submited';
+let errorMsg = '<i class="fa-solid fa-circle-xmark"></i> Error';
+let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i> You invalid';
 
 function showToast(msg) {
     let toast = document.createElement('div');
     toast.classList.add('toast');
     toast.innerHTML = msg;
     toastBox.appendChild(toast);
+ 
+    if(msg.includes('Error')) {
+        toast.classList.add('error'); 
+    } 
+    if(msg.includes('invalid')) {
+        toast.classList.add('invalid'); 
+    } 
+
+    setTimeout(()=>{
+        toast.remove();
+    }, 6000);
 }
